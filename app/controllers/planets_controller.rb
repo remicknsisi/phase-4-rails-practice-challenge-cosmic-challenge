5b2b1ceb2bb2170ca205c1c4a39f7114c1aac75d
Sienna <remicknsisi@gmail.com>
2023-04-17 13:08:45 -0400

    commit tree dda82f5e6a636ab5575d56a3f436bdf7ecf2ac11

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -7,4 +7,23 @@ class PlanetsController < ApplicationController
 
     render json: planets
   end
+
+  def show
+    render json: @planet
+  end
+
+  def update
+    @planet.update!(params)
+    render json: @planet, status: :accepted
+  end
+
+  def create
+    planet = Planet.create!(params)
+    render json: planet, status: :created
+  end
+
+  def destroy
+    @planet.destroy
+    head :no_content
+  end
 end
